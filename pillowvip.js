f69eb15fd21a5cf38e1c42b80add5f607e10e9a0
Hjalmar <130884978+BobsJohn@users.noreply.github.com>
2023-11-20 17:39:40 +0800

    Create pillowvip.js

---
+++ pillowvip.js
@@ -0,0 +1,19 @@
+#!name=Pillow
+
+#!desc=Pillow解锁会员
+
+#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/pillow.png
+
+#!homepage=https://yfamily.vercel.app
+
+#!author=CheeryTodo
+
+
+
+
+
+https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body https://raw.githubusercontent.com/BobsJohn/pillow/main/pillow.js
+
+
+
+hostname = api.revenuecat.com
